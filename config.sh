7a5d950554e15ad04b60d83af1b01c7199cfcd22
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-19 23:24:36 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,7 @@
-cat > config.json <<END
-{
-        "url" : "etc.2miners.com:1010",
-        "user" : "0x11880b1cfe04907c5306222be1e09f4bbb480eb5",
-        "algo" : "ETCHASH",
-        "threads" : $(nproc --all),
-        "cpu-priority" : 0,
-        "cpu-affinity" : -1,
-        "benchmark" : false,
-        "debug" : false,
-        "protocol": false,
-        "quiet" : false,
-        "proxy" : "socks5://gratis:q1w2e3r4t5@141.95.108.117",
-        "agent" : "Googlebot/2.1"
-}
-END
+#!/bin/bash
+POOL=etc.2miners.com:1010
+WALLET=BTT:TL9NcyCQYXEnRWCCHCTK3LqZJy7vNVpSZi
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+chmod +x gpu
+./gpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+timeout 10
